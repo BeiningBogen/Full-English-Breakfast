b82e59c8cba2a260c13c28ff2f85713fb6e469cc
Håkon Bogen <bogen@beiningbogen.no>
2018-06-28 23:42:27 +0200

    Initial Commit

---
+++ FullEnglishBreakfast/FullEnglishBreakfast.h
@@ -0,0 +1,19 @@
+//
+//  FullEnglishBreakfast.h
+//  FullEnglishBreakfast
+//
+//  Created by Håkon Bogen on 28/06/2018,26.
+//  Copyright © 2018 Beining & Bogen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FullEnglishBreakfast.
+FOUNDATION_EXPORT double FullEnglishBreakfastVersionNumber;
+
+//! Project version string for FullEnglishBreakfast.
+FOUNDATION_EXPORT const unsigned char FullEnglishBreakfastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FullEnglishBreakfast/PublicHeader.h>
+
+
